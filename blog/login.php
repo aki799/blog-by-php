199edecb4d771a73e5c2952ea1c28b987fd0e2ec
akinori <52649193+aki799@users.noreply.github.com>
2022-05-14 02:18:51 +0900

    ログイン機能画面の実装
    login.phpとsignin.cssで作成

---
+++ blog/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+
+<!-- Bootstrap core CSS -->
+<link href="./css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+ .bd-placeholder-img {
+   font-size: 1.125rem;
+   text-anchor: middle;
+   -webkit-user-select: none;
+   -moz-user-select: none;
+   user-select: none;
+ }
+
+ @media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+ }
+ </style>
+
+<!-- Custom style for this template -->
+<link href="./css/signin.css"rel="stylesheet">
+</head>
+<body class="text-center">
+
+<main class="form-signin">
+ <form>
+  <h1 class="h3 mb-3 fw-normal">ログインする</h1>
+  <label class="visually-hidden">ユーザー名</label>
+  <input type="text" name="name" class="form-control" placeholder="ユーザ名" required autofocus>
+  <label class="visually-hidden">パスワード</label>
+  <input type="password" name="password" class="form-control" placeholder="パスワード" required>
+  <button class="w-100 btn btn-lg btn-primary"type="submit">ログインする</button>
+  <p class="mt-5 mb-3 text-muted">2022</p>
+ </form>
+</main>
+    
+</body>
+</html>
