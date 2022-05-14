e6e761319de7c76bbd52a27e8a99adba3cc2b95f
akinori <52649193+aki799@users.noreply.github.com>
2022-05-15 03:31:18 +0900

    ブログ画面のデザイン
    ブログ画面のデザインを作成

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+     body {
+      padding-top: 5rem;
+     }
+     .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+     }
+
+     @media(min-width: 768px) {
+      .bd-placeholder-img-lg {
+       font-size: 3.5rem;
+      }
+     }
+    </style>
+
+    <!-- Custom style for this template -->
+    <link href="./css/blog.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+ <div class="container">
+  <a class="navbar-brand" href="/blog/">My Blog</a>
+ </div>
+</nav>
+
+<main class="container">
+ <div class="row">
+  <div class="col-md-8">
+   
+   <article class="blog-post">
+    <h2 class="blog-post-title">記事タイトル</h2>
+    <p class="blog-post-meta">2022/xx/xx</p>
+
+    <p>本文がここに入ります。</p>
+   </article>
+
+   <article class="blob-post">
+    <h2 class="blog-post-title">記事タイトル2</h2>
+    <p class="blog-post-meta">2022/xx/xx</p>
+
+    <p>本文がここに入ります。</p>
+   </article>
+
+  </div>
+
+  <div class="col-md-4">
+   <div class="p-4 mb-3 bg-light rounded">
+    <h4>ブログについて</h4>
+    <p class="mb-0">日常の出来事をつらつら書いていきます。</p>
+   </div>
+
+   <div class="p-4">
+    <h4>アーカイブ</h4>
+    <ol class="list-unstyled mb-0">
+     <li><a href="#">2022/4</a></li>
+     <li><a href="#">2022/3</a></li>
+     <li><a href="#">2022/2</a></li>
+    </ol>
+   </div>
+
+  </div>
+
+ </div><!-- /.row -->
+
+</main><!-- /.container -->
+    
+</body>
+</html>
